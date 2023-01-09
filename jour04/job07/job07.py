1af511052b2ed6397d6fb5a942c11e271e3bfea1
franck-tchassi <115140772+franck-tchassi@users.noreply.github.com>
2023-01-09 12:30:25 +0100

    Update job07.py

--- jour04/job07/job07.py
+++ jour04/job07/job07.py
@@ -1,11 +1,9 @@
-def liste():
-    L = [8,24,48,2,16]
-    n=len(L)
-    i=0
-    j=0
-    while n>i:
-        if L[i] % 3 == 0:
-            j+=1
-        i+=1
-    print(j)
-liste()
+def Nbr_multipre_de_trois():
+    compteur = 0
+    L = [8, 24,48,2,16]
+    for i in L:
+        if i % 3 == 0:
+            compteur +=1
+    print(compteur)
+
+Nbr_multipre_de_trois()

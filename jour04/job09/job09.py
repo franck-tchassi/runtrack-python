69842ca3e1a62d8fcc26cf617b5b48a598fd4a2e
franck-tchassi <115140772+franck-tchassi@users.noreply.github.com>
2023-01-09 13:53:37 +0100

    Update job09.py

--- jour04/job09/job09.py
+++ jour04/job09/job09.py
@@ -1,19 +1,15 @@
-def liste():
-    L = [8,24,27,48,2,16,9,102,7,84,91]
-    n=len(L)
-    i=0
-    min= L[0]
-    max=0
-    while n > i:
-        if min > L[i] and max < L[i]:
-            min= L[i]
-            max= L[i]
-        elif min > L[i]:
-            min = L[i]
-        elif max < L[i]:
-            max = L[i]
-        i+=1
-    print(min)
-    print(max)
+def max_and_min():
+    L = [8, 24, 27, 48, 2,16, 9, 102, 7, 84, 91]
+    min = L[0]
+    max = L[0]
+    for i in L:
+        if i < min:
+            min = i
+        if i > max:
+            max = i 
+    sum = min + max
+    print("le maximum est:" +''+ str(max))
+    print("le minimun est:" +''+ str(min))
+    print("la somme est:", sum)
 
-liste()
+max_and_min()

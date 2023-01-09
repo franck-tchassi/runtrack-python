450e620d8a21d71b606190777581bff1d25cac8e
franck-tchassi <115140772+franck-tchassi@users.noreply.github.com>
2023-01-09 15:01:03 +0100

    Update job010.py

--- jour04/job010/job010.py
+++ jour04/job010/job010.py
@@ -1,9 +1,8 @@
-def liste():
-    L = [8, 24, 27, 48, 2, 16, 9, 102, 7, 84, 91]
-    produit=1
-    for x in L:
-        if x>=25 and x<=90:
-            produit= produit * x
-    print(produit)
-
-liste()
+def produits():
+    L = [8, 24, 27, 48, 2,16, 9, 102, 7, 84, 91]
+    produit = 1
+    for i in L:
+        if 25 <= i <= 90:
+            produit *= i
+    print("le produit est de:",produit)
+produits()
